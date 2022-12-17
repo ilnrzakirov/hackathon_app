7c6e2772aa0f1f9c6f3ef418300227f13fefbfdc
Ilnur <ilnrzakirov@gmail.com>
2022-12-17 19:57:02 +0300

    Добавил модели БД

--- traffic_app/event_traffic/admin.py
+++ traffic_app/event_traffic/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Student, Feedback, Register
+
 
 # Register your models here.
+
+
 class EventAdmin(admin.ModelAdmin):
     readonly_fields = ["link"]
     list_display = ("name", "link")
+    search_fields = ["name"]
+    list_filter = ["type"]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["name", "email"]
+    search_fields = ["name"]
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ["student", "event", "feedback"]
+
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ["student", "event", "event_check", "link"]
 
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Register, RegisterAdmin)
--- traffic_app/event_traffic/models.py
+++ traffic_app/event_traffic/models.py
@@ -14,19 +14,19 @@ class Event(models.Model):
     name = models.CharField(verbose_name="Название мероприятия",
                             null=False,
                             blank=False,
-                            max_length=500,)
+                            max_length=500, )
     type = models.IntegerField(choices=EVENTS_TYPE,
                                verbose_name="Тип мероприятия",
                                default=2,
                                null=False,
-                               blank=False,)
+                               blank=False, )
     description = models.TextField(verbose_name="Описание",
                                    blank=True,
-                                   null=True,)
+                                   null=True, )
     link = models.CharField(verbose_name="Ссылка регистрации",
                             blank=True,
                             null=True,
-                            max_length=500,)
+                            max_length=500, )
 
     class Meta:
         verbose_name = "Мероприятия"
@@ -34,4 +34,67 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(verbose_name="Имя студента",
+                            null=False,
+                            blank=False,
+                            max_length=100, )
+    email = models.EmailField(verbose_name="Email",
+                              null=False,
+                              blank=False)
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    student = models.ForeignKey(Student,
+                                verbose_name="Студент",
+                                on_delete=models.CASCADE,
+                                null=False, )
+    event = models.ForeignKey(Event,
+                              verbose_name="Мероприятие",
+                              on_delete=models.CASCADE,
+                              null=False, )
+    feedback = models.IntegerField(verbose_name="Оценка",
+                                   null=False,
+                                   blank=False, )
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+    def __str__(self):
+        return f"{self.student} - {self.event} - {self.feedback}"
+
+
+class Register(models.Model):
+    student = models.ForeignKey(Student,
+                                verbose_name="Студент",
+                                on_delete=models.CASCADE,
+                                null=False, )
+    event = models.ForeignKey(Event,
+                              verbose_name="Мероприятие",
+                              on_delete=models.CASCADE,
+                              null=False, )
+    event_check = models.BooleanField(default=False,
+                                      verbose_name="Посетил?")
+    link = models.CharField(max_length=500,
+                            verbose_name="Уникальная ссылка для отзыва",
+                            null=True,
+                            blank=True, )
+
+    class Meta:
+        verbose_name = "Регистрация на мероприятие"
+        verbose_name_plural = "Регистрации на мероприятия"
+
+    def __str__(self):
+        return f"{self.student} - {self.event}"
 
